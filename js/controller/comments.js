d2e8cec586399f50ce2d023f8b16799fccb070e2
johnnyyvard <johnnyyvard@me.com>
2020-12-30 15:14:35 +0100

    Les filtres

--- js/controller/comments.js
+++ js/controller/comments.js
@@ -1,34 +1,28 @@
-app.controller('CommentsCtrl', function ($scope, $rootScope, Post, $routeParams) {
-
+app.controller('CommentsCtrl', function ($scope, $rootScope, Post, $routeParams, $filter) {
     $rootScope.loading = true;
     $scope.newComment = {};
-
+    $scope.chiffre = $filter('round')(2.879456123, 2);
+    $scope.search = {};
     Post.get($routeParams.id).then(function (post) {
-
         $rootScope.loading = false;
         $scope.title = post.name;
         $scope.comments = post.comments;
-
     }, function (msg) {
-
         alert(msg);
-
-    })
-
+    });
     $scope.addComment = function () {
-
         $scope.comments.push($scope.newComment);
-
         Post.add($scope.newComment).then(function () {
-
         }, function () {
-
             alert('Votre message n\'a pas pu être sauvegardé');
-
-        })
-
+        });
         $scope.newComment = {};
+    };
+});
 
-    }
-
+app.filter('round', function () {
+    return function (input, precision) {
+        chiffre = Math.round(input * Math.pow(10, precision)) / Math.pow(10, precision);
+        return chiffre.toString().replace('.', ',') ;
+    };
 });
