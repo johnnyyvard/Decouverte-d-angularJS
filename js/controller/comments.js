6d3bafdc7d04ad6a836106fa81a214d7621dcca2
johnnyyvard <johnnyyvard@me.com>
2020-12-30 10:12:40 +0100

    Fin de la partie sur les promesses

--- js/controller/comments.js
+++ js/controller/comments.js
@@ -1,24 +1,34 @@
-app.controller('CommentsCtrl', function ($scope, Post, $routeParams) {
+app.controller('CommentsCtrl', function ($scope, $rootScope, Post, $routeParams) {
 
-    $scope.loading = true;
+    $rootScope.loading = true;
     $scope.newComment = {};
 
     Post.get($routeParams.id).then(function (post) {
-        $scope.loading = false;
+
+        $rootScope.loading = false;
         $scope.title = post.name;
         $scope.comments = post.comments;
+
     }, function (msg) {
+
         alert(msg);
+
     })
 
-    $scope.addComment = function(){
-        $scope.comments.push($scope.newComment);
-        Post.add($scope.newComment).then(function(){
+    $scope.addComment = function () {
 
-        }, function(){
+        $scope.comments.push($scope.newComment);
+
+        Post.add($scope.newComment).then(function () {
+
+        }, function () {
+
             alert('Votre message n\'a pas pu être sauvegardé');
+
         })
+
         $scope.newComment = {};
+
     }
 
 });
--- js/controller/posts.js
+++ js/controller/posts.js
@@ -1,10 +1,15 @@
 app.controller('PostCtrl', function ($scope, $rootScope, Post) {
+
     $rootScope.loading = true;
     $scope.posts = Post.find().then(function (posts) {
+
         $rootScope.loading = false;
         $scope.posts = posts;
+
     }, function (msg) {
+
         alert(msg);
+        
     });
 
 })
